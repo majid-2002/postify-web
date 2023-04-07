003c0c1c86ae5f441693bb85fdcf52f8dd6d4d61
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-07 18:27:35 +0530

    update history area

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -2,37 +2,39 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 
 function HistoryCard() {
-
-  return(
-  <div className="history-card rounded-2">
-    <Row>
-      <Col>
-        <h3>GET</h3>
-      </Col>
-      <Col>
-        <p>https://google.com/favicon.ico</p>
-        <p>
-          <span>21:15</span>
-          <span>21 March 2023</span>
-        </p>
-      </Col>
-    </Row>
-  </div>
-  )
+  return (
+    <div className="history-card rounded-2">
+      <Row>
+        <Col>
+          <h3>GET</h3>
+        </Col>
+        <Col>
+          <p>https://google.com/favicon.ico</p>
+          <p>
+            <span>21:15</span>
+            <span>21 March 2023</span>
+          </p>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 function History() {
   return (
     <div className="history">
-      <h3>History</h3>
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <div className="history-text-area">
+        <h2>History</h2>
+      </div>
+      <div className="history-card-area">
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+      </div>
     </div>
   );
 }
