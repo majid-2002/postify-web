d912d1fdcc9853bc578e9e93fc038f0d1f54b794
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-12 11:03:13 +0530

    langtype set for codmirror

--- src/App.js
+++ src/App.js
@@ -9,33 +9,63 @@ import prettier from "prettier/standalone";
 import parserHtml from "prettier/parser-html";
 
 function App() {
-  const [responseData, setResponseData] = useState("");
+  const [responseData, setResponseData] = useState({
+    data: "",
+    lang_type: "",
+  });
 
   const handleSubmitRequest = async () => {
     const response = await makeApiCall(endpoint);
     const contentType = response.headers["content-type"];
     console.log(contentType);
-
-    let stringValue = response.data;
+    let stringValue;
 
     if (contentType.includes("json")) {
       stringValue = JSON.stringify(response.data, null, 2);
+      setResponseData({
+        ...responseData,
+        data : stringValue,
+        lang_type: "json",
+      });
     } else if (contentType.includes("html")) {
       stringValue = prettier.format(response.data, {
         parser: "html",
         plugins: [parserHtml],
       });
+      setResponseData({
+        ...responseData,
+        data : stringValue,
+        lang_type: "html",
+      });
     } else if (contentType.includes("xml")) {
       stringValue = prettier.format(response.data, { parser: "xml" });
+      setResponseData({
+        ...responseData,
+        data : stringValue,
+        lang_type: "xml",
+      });
     } else if (contentType.includes("javascript")) {
       stringValue = prettier.format(response.data, { parser: "babel" });
+      setResponseData({
+        ...responseData,
+        data : stringValue,
+        lang_type: "javascript",
+      });
     } else {
-      // Assume plain text
       stringValue = prettier.format(response.data, { parser: "text" });
+      setResponseData({
+        ...responseData,
+        data : stringValue,
+        lang_type: "text",
+      });
     }
 
-    setResponseData(stringValue);
+    
   };
+
+
+
+
 
   const [endpoint, setEndpoint] = React.useState({
     url: "",
--- src/components/ResponseArea.js
+++ src/components/ResponseArea.js
@@ -3,8 +3,14 @@ import CodeMirror from "@uiw/react-codemirror";
 import { dracula } from "@uiw/codemirror-theme-dracula";
 import { html } from "@codemirror/lang-html";
 import { Col, Row } from "react-bootstrap";
+import { json } from "@codemirror/lang-json";
+import { javascript } from "@codemirror/lang-javascript";
+import { xml } from "@codemirror/lang-xml";
 
 export default function ResponseArea({responseData}) {
+
+  const contentType = responseData.lang_type;
+
   return (
     <div className="response-area px-3">
       <Row className="result-container pt-2 w-25">
@@ -23,12 +29,22 @@ export default function ResponseArea({responseData}) {
       </Row>
       <Row>
         <CodeMirror
-          value={responseData}
+          value={responseData.data}
           readOnly={true}
           placeholder={"Your response goes here."}
           theme={dracula}
-          extensions={[html()]}
           height="40vh"
+          extensions={
+              contentType === "json"
+                ? [json()]
+                : contentType === "javascript"
+                ? [javascript()]
+                : contentType === "html"
+                ? [html()]
+                : contentType === "xml"
+                ? [xml()]
+                : []
+            }
         />
       </Row>
     </div>
