3db6d92973ecdeb6a7001f86b51ccb01dcb1923b
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-05-10 14:07:18 +0530

    Update icon

--- src/components/HeaderArea.jsx
+++ src/components/HeaderArea.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function HeaderArea() {
   return (
     <div className="header-area">
+      <img src='/images/icon.png' alt='postify-logo'></img>
       <h1>Postify</h1>
     </div>
   )
